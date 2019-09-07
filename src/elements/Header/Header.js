d4d59c9a791ac36b86dcef6edf8dfe7c07b3bab7
jvinals <jvinals2@yahoo.com>
2019-09-07 15:10:18 -0500

    Added first scaffold

---
+++ src/elements/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return(
+        <div className="series-header">
+            <div className="series-header-content">
+                <img className="series-logo" src="./images/logo.png" alt="series-logo" />
+            </div>
+        </div>
+    )
+}
+
+export default Header;
