5fadb831de041b22c257ac97f8e4898ea89184cf
jvinals <jvinals2@yahoo.com>
2019-09-14 12:25:28 -0500

    Added config file

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './Home.css';
 import Tabla from '../elements/Tabla/Tabla';
+import {API_URL, API_KEY, IMAGE_BASE_URL, BACKDROP_SIZE, POSTER_SIZE} from '../config';
 
 class Home extends React.Component {
     state ={
 
     }
-
+    
     render (){
             return (
                 <div className="series-home">
