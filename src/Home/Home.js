42e069829dd71699222f23c18f7db16a2fa19772
jvinals <jvinals2@yahoo.com>
2019-09-14 16:44:51 -0500

    Modificaciones variadas

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -25,9 +25,12 @@ class Home extends React.Component {
                     picSerie: resultJSON.results[0]
                 })
                 console.log ("DATA FETCHED:");
-                console.log(resultJSON);
+                console.log(resultJSON.results);
+                console.log("Is it an Array?: "+Array.isArray(resultJSON.results));
+                if (Array.isArray(resultJSON.results)) {console.log("Length: "+resultJSON.results.length)};
             })
     }
+    
 
     render (){
             console.log("ESTADO:");
@@ -42,8 +45,11 @@ class Home extends React.Component {
                         dallas={this.state.dallas}
                     />
                     {console.log(this.state.series[0])}
-                    {this.state.series.map( () => {
+                    {this.state.series.map( (item,key) => {
                         console.log('A');
+                        console.log("Is (now 'THIS') it an Array?: "+Array.isArray(this.state.series[0]));
+                        console.log(this.state.series[0]);
+                        item.map( (item,key) =>  console.log(key))
                     }
                         
                     )
