674d5a07a2c1e4283fdf1a38b1f13f94071e9fff
jvinals <jvinals2@yahoo.com>
2019-09-15 15:56:16 -0500

    Array iteration working

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -11,9 +11,9 @@ class Home extends React.Component {
         dallas: 6
     }
     
-    
+
     componentDidMount(){
-        const endpoint = `${API_URL}search/tv?api_key=${API_KEY}&page=1&query=Blacklist`;
+        const endpoint = `${API_URL}search/tv?api_key=${API_KEY}&page=1&query=black`;
         this.fetchItems(endpoint);
     }
 
@@ -25,40 +25,35 @@ class Home extends React.Component {
                     series:[resultJSON.results],
                     picSerie: resultJSON.results[0]
                 })
-                console.log ("DATA FETCHED:");
-                console.log(resultJSON.results);
-                console.log("Is it an Array?: "+Array.isArray(resultJSON.results));
-                if (Array.isArray(resultJSON.results)) {console.log("Length: "+resultJSON.results.length)};
             })
     }
     
 
     render (){
-            console.log("ESTADO:");
-            console.log(this.state.picSerie.original_name);
+            var rowSeries;
+            const rowObject = this.state.series.map((row, index) => {
+                rowSeries = row.map((row2, index) => {
+                    return (
+                        <div>   
+                        <Serie 
+                            image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${row2.backdrop_path}`}
+                            titulo={row2.original_name}
+                            season='x'
+                            madrid={this.state.madrid}
+                            dallas={this.state.dallas}
+                        />                            
+                        </div>
+                    );
+                });
+            });
+            
             return (
                 <div className="series-home">
-                    <Serie 
-                        image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${this.state.picSerie.backdrop_path}`}
-                        titulo={this.state.picSerie.original_name}
-                        season='x'
-                        madrid={this.state.madrid}
-                        dallas={this.state.dallas}
-                    />
-                    {console.log(this.state.series[0])}
-                    {this.state.series.map( (item,key) => {
-                        console.log('A');
-                        console.log("Is (now 'THIS') it an Array?: "+Array.isArray(this.state.series[0]));
-                        console.log(this.state.series[0]);
-                        item.map( (item,key) =>  console.log(key))
-                    }
-                        
-                    )
-
-                    }
+                    {rowSeries}
                 </div>
             )
-    }
+            
+        }
 }
 
 export default Home;
@@ -81,4 +76,12 @@ export default Home;
                 })
             })
     }
+*/
+
+
+/*
+                console.log ("DATA FETCHED:");
+                console.log(resultJSON.results);
+                console.log("Is it an Array?: "+Array.isArray(resultJSON.results));
+                if (Array.isArray(resultJSON.results)) {console.log("Length: "+resultJSON.results.length)};
 */
