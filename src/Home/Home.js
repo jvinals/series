599bbe62f2b82167b3484a22a7a599d548476ded
jvinals <jvinals2@yahoo.com>
2019-09-22 10:25:30 -0500

    Refactor: finsih lifting state up to Home Comp

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -2,12 +2,16 @@ import React from 'react';
 import './Home.css';
 import {API_URL, API_KEY, IMAGE_BASE_URL, BACKDROP_SIZE, POSTER_SIZE} from '../config';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 import Serie from '../elements/Serie/Serie';
 import SearchList from '../elements/SearchList/SearchList';
 
 class Home extends React.Component {
     state ={
-        series :[],
+        series : [],
+        resultSearch: [],
+        inputText: '',
         madrid: 4,
         dallas: 6
     }
@@ -23,13 +27,51 @@ class Home extends React.Component {
         fetch(endpoint)
         .then(result => result.json())  
         .then(result => {
-            
                 this.setState({
                     series:result,
                 })
             })
     }
+    // Functions for Search Box
+    updateInput = (evt) => {
+        console.log('Input Text: '+evt.target.value);
+        this.setState({
+            inputText: evt.target.value
+        })  
+    }
+    getSeries = () => {
+        const endpoint = `${API_URL}search/tv?api_key=${API_KEY}&page=1&query=${this.state.inputText}`;
+        this.fetchSearchItems(endpoint);    
+        /*
+        this.setState({
+            //heightBox:'180px'
+        })
+        */ 
+    }
 
+    fetchSearchItems = (endpoint) => {
+        fetch(endpoint)
+        .then(result => result.json())  
+        .then(result => {     
+                this.setState({
+                    resultSearch:result.results,
+                })
+            })
+    }
+
+    addSerie = () => {
+        console.log('addSerie');
+        this.setState({
+            resultSearch:[],
+            // inputText:'',
+        })
+    }
+    
+    renderSearchBox = () => {
+
+    }
+    // (END) Functions for Search Box
+    
     render (){
             const rowSeries = this.state.series.map((row, index) => {
                 console.log('Row '+index+' '+row.nombre);
@@ -48,15 +90,52 @@ class Home extends React.Component {
             });
             
             console.log(rowSeries);
+            const searchListContent = this.state.resultSearch.map((row, index) => {
+                return(
+                    <div>
+
+                        <div className='RowResult' onClick={this.addSerie} > 
+                        <div className="imgSerie"
+                                style={{
+                                    height:'150px',
+                                    width:'300px',
+                                    marginRight:'10px',
+                                    marginTop:'5px',
+                                    marginBottom:'5px',
+                                    backgroundSize: '100% 100%',
+                                    backgroundColor: 'rbga(100,100,100,.5)',
+                                    backgroundImage:`url('${IMAGE_BASE_URL}${BACKDROP_SIZE}${row.backdrop_path}')`,
+                                }} 
+                            ></div>  
+                            <div className="Titulo">{row.name}</div>
+                            <div className="Fecha">{row.first_air_date.substring(0,4)}</div>
+                            <div className="Descripcion">{row.overview}</div>
+                        </div>
+   
+                    </div>
+                    );
+            });
+
+            console.log('search list content: '+searchListContent);
+
             return (
                 <div>
-                    <div>
-                        <SearchList/>
+                    <div className="SearchArea" style={{height:this.state.heightBox}}>
+                        <Button onClick={this.getSeries} variant="outline-primary" style={{marginLeft:'0px', float:'left'}}>Buscar Serie
+                        </Button>
+                        <Form.Control type="email" placeholder="Nombre de la serie" onChange={this.updateInput} style={{width:'300px', float:'left', marginLeft:'20px'}}/>
+                        <div className="ResultContainer">
+                        <div>
+                             {searchListContent}
+                        </div>
+                        </div>
                     </div>
+
                     <div className="loc1">Madrid</div><div className="loc2">Dallas</div>
                     <div className="series-home">
                         {rowSeries}
                     </div>
+
                 </div>
                 
             )
--- src/elements/SearchList/SearchList.js
+++ src/elements/SearchList/SearchList.js
@@ -77,16 +77,8 @@ class SearchList extends React.Component {
         });
 
         return(
-            <div>
-
-            <div className="SearchArea" style={{height:this.state.heightBox}}>
-                <Button onClick={this.getSeries} variant="outline-primary" style={{marginLeft:'0px', float:'left'}}>Buscar Serie
-                </Button>
-                <Form.Control type="email" placeholder="Nombre de la serie" onChange={this.updateInput} style={{width:'300px', float:'left', marginLeft:'20px'}}/>
-                <div className="ResultContainer">
-                    <div>{rowSearch}</div>
-                </div>
-            </div>
+            <div className="ResultContainer">
+                <div>{rowSearch}</div>
             </div>
         )
     
